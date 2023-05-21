d70d3c7f891bf578df4656c5c0010966bec66409
SunYasmin <qyaseminx@gmail.com>
2023-05-21 06:52:47 +0300

    Car-Inheritance

---
+++ src/main/java/day22inheritancepolymorphism/VehicleRunner.java
@@ -0,0 +1,14 @@
+package day22inheritancepolymorphism;
+
+public class VehicleRunner {
+
+    public static void main(String[] args) {
+
+
+        //Example 1: Car turunde rengi beyaz olan 54.000 km de 250000 tl fiyati olan 2020 model Honda Accord ariyorum
+
+        HondaAccord hacc=new HondaAccord(250000,2020);
+
+    }
+
+}
