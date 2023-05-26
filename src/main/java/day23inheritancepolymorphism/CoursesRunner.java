c3d3009cebb3bebcc9d864d85da84da354d7607e
SunYasmin <qyaseminx@gmail.com>
2023-05-26 21:48:13 +0300

    constructor

---
+++ src/main/java/day23inheritancepolymorphism/CoursesRunner.java
@@ -0,0 +1,16 @@
+package day23inheritancepolymorphism;
+
+public class CoursesRunner {
+
+    public static void main(String[] args) {
+
+//        Math math = new Math();
+//        math.liveSessions();
+//        math.practice();
+
+        Math math = new Math();
+
+
+    }
+
+}
