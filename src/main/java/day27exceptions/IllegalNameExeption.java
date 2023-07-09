d93c1128c8428b01f08525008f6e650d8f081452
SunYasmin <qyaseminx@gmail.com>
2023-07-09 14:25:44 +0300

    exc

---
+++ src/main/java/day27exceptions/IllegalNameExeption.java
@@ -0,0 +1,8 @@
+package day27exceptions;
+
+public class IllegalNameExeption extends RuntimeException{
+
+        public IllegalNameExeption(String message){
+            super(message);
+        }
+}
